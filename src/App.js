f22ba2828bfa6974009c429ac9c279a18febcfa3
leela <info@redmindstech.com>
2021-07-03 03:14:22 +0530

    node update, creating new component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+
 
 function App() {
+  const expenses = [
+    {id:'e1',title:'Car Insurance', amount:255.85, date:new Date(2021,5,20)},
+    {id:'e2',title:'Bike Insurance', amount:255.85, date:new Date(2021,2,6)},
+    {id:'e3',title:'New Tv', amount:266.85, date:new Date(2021,7,19)},
+    {id:'e4',title:'Sofa', amount:245.85, date:new Date(2021,42,1)}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Expenses expenses={expenses} />
   );
 }
 
